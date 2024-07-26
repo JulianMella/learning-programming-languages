81df81931ef5260219a3e82b17d58f3317c80b0f
JulianMella <julianmella2@gmail.com>
2024-07-26 14:34:03 -0700

    Struct example

--- Rust/Projects/Rust Programming Language Book/chapter 5 structs/src/main.rs
+++ Rust/Projects/Rust Programming Language Book/chapter 5 structs/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+/*
+Enum and structs are the building blocks for creating new types in Rust
+
+Chapter 5 covers coupling related data using structs also cover using methods and
+associated functions on structs and compare them to tuples
+
+
+
+*/
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
+
+// Like a tuple, our struct allows us to group related data together of different types.
+// The benefit is that we can name our structure and we also can name the data inside
+// Easier to reference data by name than index location.
+
+fn main() {}
